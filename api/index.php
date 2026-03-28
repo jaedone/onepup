071f385bb9bb277d4d3c468e85a9b887933ff4a8
Mark Joseph <neypesmarkjosephb@gmail.com>
2026-03-28 19:58:43 +0800

    Fix Vercel Laravel runtime config

--- api/index.php
+++ api/index.php
@@ -1,15 +1,14 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+$request = Illuminate\Http\Request::capture();
 
+$response = $kernel->handle($request);
 $response->send();
 
 $kernel->terminate($request, $response);
